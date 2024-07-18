0fa90d6e55f7e0a5e5a7d849efbc5ce1fcf51646
ioedumac <eacevedof@yahoo.es>
2024-07-19 00:03:48 +0200

    example command and composer pint

---
+++ app/Console/Commands/Devops/PintCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Console\Commands\Devops;
+
+use Illuminate\Console\Command;
+
+final class PintCommand extends Command
+{
+    protected $signature = "app:pint-command";
+    protected $description = "Command description";
+
+    public function handle()
+    {
+        //
+    }
+}
