ea455e911a35a5baddfc524ed5ac018caed2c808
ioedumac <eacevedof@yahoo.es>
2024-07-19 00:02:36 +0200

    quito api-old.php

--- routes/api-old.php
+++
@@ -1,8 +0,0 @@
-<?php
-//api.php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HealthCheck\GetHealthCheckController;
-
-Route::get("/api/health-check", GetHealthCheckController::class);
-
