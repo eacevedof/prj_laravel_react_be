d3e4e1acf543847f8fc91399efcc5865b7c0c2fa
ioedumac <eacevedof@yahoo.es>
2024-07-27 19:03:50 +0200

    sys-user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,36 +9,43 @@ use Illuminate\Support\Facades\Schema;
 return new class () extends Migration {
     public function up(): void
     {
-        Schema::create("users", function (Blueprint $table): void {
+        Schema::create("sys_user", function (Blueprint $table): void {
             $table->id();
-            $table->string("name");
-            $table->string("email")->unique();
+            $table->string("created_platform", 3)->nullable();
+            $table->string("created_by", 10)->nullable();
+            $table->dateTime("created_at") ->nullable();
+
+            $table->string("updated_platform", 3)->nullable();
+            $table->string("updated_by", 10)->nullable();
+            $table->dateTime("updated_at") ->nullable();
+
+            $table->string("deleted_platform", 3)->nullable();
+            $table->string("deleted_by", 10)->nullable();
+            $table->dateTime("deleted_at") ->nullable();
+
+            $table->string("uuid", 50)->unique();
+            $table->string("username", 50)->unique();
+            $table->string("secret_pwd", 100);
+            $table->string("reset_pwd_token")->unique()->nullable();
+            $table->string("first_name", 50)->nullable();
+            $table->string("first_surname", 50)->nullable();
+
             $table->timestamp("email_verified_at")->nullable();
-            $table->string("password");
-            $table->rememberToken();
-            $table->timestamps();
         });
 
-        Schema::create("password_reset_tokens", function (Blueprint $table): void {
-            $table->string("email")->primary();
-            $table->string("token");
-            $table->timestamp("created_at")->nullable();
-        });
+        Schema::create("app_user", function (Blueprint $table): void {
+            $table->id();
 
-        Schema::create("sessions", function (Blueprint $table): void {
-            $table->string("id")->primary();
-            $table->foreignId("user_id")->nullable()->index();
-            $table->string("ip_address", 45)->nullable();
-            $table->text("user_agent")->nullable();
-            $table->longText("payload");
-            $table->integer("last_activity")->index();
+            $table->integer("sys_user_id")->unsigned();
+
+            $table->string("email", 100)->nullable();
+            $table->string("middle_name", 50)->nullable();
+            $table->string("second_surname", 50)->nullable();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists("users");
-        Schema::dropIfExists("password_reset_tokens");
-        Schema::dropIfExists("sessions");
+        Schema::dropIfExists("base_user");
     }
 };
