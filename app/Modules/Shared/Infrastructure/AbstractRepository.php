efbfdd7c96cfd1167f14d00f95d687651bc8d78c
ioedumac <eacevedof@yahoo.es>
2024-07-27 21:14:51 +0200

    abstract repository

---
+++ app/Modules/Shared/Infrastructure/AbstractRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Shared\Infrastructure;
+
+use Illuminate\Support\Facades\DB;
+
+final class AbstractRepository
+{
+    protected function query(string $sql): array
+    {
+        return DB::select($sql);
+    }
+
+    protected function command(string $table): object
+    {
+        return DB::table($table);
+    }
+
+}
