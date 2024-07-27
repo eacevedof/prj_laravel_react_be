dd7b71ba8d9d4d04ef595f9b363e3ffc0371100f
ioedumac <eacevedof@yahoo.es>
2024-07-27 21:29:07 +0200

    create user use-case

---
+++ app/Modules/Users/CreateUser/Application/CreateUserService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Modules\Users\CreateUser\Application;
+
+final class CreateUserService
+{
+
+    public function __invoke(CreateUserDto $createUserDto): CreatedUserDto
+    {
+
+    }
+}
---
+++ app/Modules/Users/CreateUser/Application/CreatedUserDto.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Modules\Users\CreateUser\Application;
+
+final readonly class CreatedUserDto
+{
+    public function __construct(array $primitives)
+    {
+
+    }
+
+    public function fromPrimitives(array $primitives): self
+    {
+        return new self($primitives);
+    }
+}
