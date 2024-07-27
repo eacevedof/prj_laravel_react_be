7d9769cdb7688565dea3a22e036303e2811fb974
ioedumac <eacevedof@yahoo.es>
2024-07-28 00:08:56 +0200

    create-user

---
+++ app/Modules/Shared/Infrastructure/Traits/JsonResponseTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Modules\Shared\Infrastructure\Traits;
+
+use Illuminate\Http\JsonResponse;
+trait JsonResponseTrait
+{
+    private function getJsonResponse(
+        array $data = [],
+        int $status = 200,
+        array $headers = [],
+    ): JsonResponse
+    {
+        return new JsonResponse($data, $status, $headers, 0);
+    }
+}
--- app/Modules/Users/CreateUser/Infrastructure/CreateUserController.php
+++ app/Modules/Users/CreateUser/Infrastructure/CreateUserController.php
@@ -5,17 +5,22 @@ namespace App\Modules\Users\CreateUser\Infrastructure;
 use App\Modules\Users\CreateUser\Application\CreateUserDto;
 use App\Modules\Users\CreateUser\Application\CreateUserService;
 use Illuminate\Http\Request;
+use App\Modules\Shared\Infrastructure\Traits\JsonResponseTrait;
+use Illuminate\Http\JsonResponse;
 
 final readonly class CreateUserController
 {
+    use JsonResponseTrait;
+
     public function __construct(
         private CreateUserService $createUserService
     ) {
     }
 
-    public function __invoke(Request $httpRequest): void
+    public function __invoke(Request $httpRequest): JsonResponse
     {
         $createUserDto = CreateUserDto::fromPrimitives($httpRequest->all());
         $this->createUserService->__invoke($createUserDto);
+        return $this->getJsonResponse([], 201);
     }
 }
