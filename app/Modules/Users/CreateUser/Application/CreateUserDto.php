bf410348e4efc117c7939f216297d678ab547fb2
ioedumac <eacevedof@yahoo.es>
2024-07-27 23:03:11 +0200

    create-user-dto

--- app/Modules/Users/CreateUser/Application/CreateUserDto.php
+++ app/Modules/Users/CreateUser/Application/CreateUserDto.php
@@ -2,8 +2,30 @@
 
 namespace App\Modules\Users\CreateUser\Application;
 
-final class CreateUserDto
+final readonly class CreateUserDto
 {
-
+    public ?string $createdPlatform;
+    public ?string $createdBy;
+    public ?\DateTime $createdAt;
+    public ?string $updatedPlatform;
+    public ?string $updatedBy;
+    public ?\DateTime $updatedAt;
+    public ?string $deletedPlatform;
+    public ?string $deletedBy;
+    public ?\DateTime $deletedAt;
+    public bool $isEnabled;
+    public int $id;
+    public string $uuid;
+    public string $username;
+    public string $secretPwd;
+    public ?string $secretPwdReset;
+    public ?\DateTime $verifiedAt;
+    public int $sysUserId;
+    public ?string $email;
+    public ?string $firstName;
+    public ?string $firstSurname;
+    public ?string $mobileNumber;
+    public ?string $middleName;
+    public ?string $secondSurname;
 
 }
