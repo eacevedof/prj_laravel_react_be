5cc0f9677572a770d718fe2179e107309e91d5c3
ioedumac <eacevedof@yahoo.es>
2024-07-27 21:19:26 +0200

    abstract repository

--- app/Modules/Users/Domain/Entities/UserEntity.php
+++ app/Modules/Users/Domain/Entities/UserEntity.php
@@ -4,32 +4,7 @@ declare(strict_types=1);
 
 namespace App\Modules\Users\Domain\Entities;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-final class UserEntity extends Authenticatable
+final class UserEntity
 {
-    use HasFactory;
-    use Notifiable;
 
-    protected $fillable = [
-        "name",
-        "email",
-        "password",
-    ];
-
-    protected $hidden = [
-        "password",
-        "remember_token",
-    ];
-
-    protected function casts(): array
-    {
-        return [
-            "email_verified_at" => "datetime",
-            "password" => "hashed",
-        ];
-    }
 }
