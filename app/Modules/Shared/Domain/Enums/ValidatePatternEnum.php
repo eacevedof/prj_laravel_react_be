7ca66e5d58f77f999e749d574a90b34d70b5e52c
ioedumac <eacevedof@yahoo.es>
2024-07-28 23:09:07 +0200

    create-user validation

--- app/Modules/Shared/Domain/Enums/ValidatePatternEnum.php
+++ app/Modules/Shared/Domain/Enums/ValidatePatternEnum.php
@@ -6,7 +6,7 @@ namespace App\Modules\Shared\Domain\Enums;
 
 enum ValidatePatternEnum: string
 {
-    case EMAIL = "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
-    case PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$";
+    case EMAIL = "^[\\w\\.-]+@[\\w\\.-]+\\.[a-zA-Z]{2,4}$";
+    case PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$";
     case NAME = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$";
 }
