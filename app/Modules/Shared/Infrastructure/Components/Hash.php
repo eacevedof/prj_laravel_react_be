16453484cb329e5dfc986fd3b2524e86a93b79b5
ioedumac <eacevedof@yahoo.es>
2024-07-28 18:05:48 +0200

    hash password

--- app/Modules/Shared/Infrastructure/Components/Hash.php
+++ app/Modules/Shared/Infrastructure/Components/Hash.php
@@ -10,6 +10,21 @@ final class Hash
 {
     public static function getHashResult(string $string): string
     {
+        /*
+        use Illuminate/Hashing/ArgonHasher::make()
+        $hash = @password_hash($value, $this->algorithm(), [
+            'memory_cost' => $this->memory($options),
+            'time_cost' => $this->time($options),
+            'threads' => $this->threads($options),
+        ]);
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        if (password_verify($password, $hashedPassword)) {
+         * */
         return IlluminateHash::make($string);
+    }
+
+    public static function doesStringHaveThisHash(string $string, string $hash): bool
+    {
+        return IlluminateHash::check($string, $hash);
     }
 }
